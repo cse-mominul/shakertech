8612b4f83831a916403c45f08b140f9ac7210c3e
cse-mominul <cse.mominul.islam>
2023-05-17 20:23:16 +0600

    added some content

--- src/Layout/Main/ServicesD.js
+++ src/Layout/Main/ServicesD.js
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 
 const ServicesD = () => {
   const { id } = useParams();
-  
+
   const [activeTab, setActiveTab] = useState(parseInt(id));
   const handleTabClick = (tabNumber) => {
     setActiveTab(tabNumber);
@@ -61,7 +61,7 @@ const ServicesD = () => {
         </div>
       </section>
 
-      <div class="flex lg:ml-40 lg:mr-40">
+      <div class="flex lg:ml-40 lg:mr-40 border-green-500">
         <button
           onClick={() => handleTabClick(1)}
           className={` ${
@@ -119,27 +119,73 @@ const ServicesD = () => {
               <h1 className="text-3xl mb-2">What is reverse engineering?</h1>
               <p>
                 Reverse engineering is taking apart a computer program to see
-                how it works in order to duplicate or enhance the software. It
-                can be used for:
+                how it works in order to duplicate <br /> or enhance the
+                software. It can be used for:
               </p>
+              <ul className="list-disc ml-6 text-gry-500">
+                <li>
+                  source code recovery
+                  <li>malware analysis </li>
+                  <li>software localization</li>
+                </li>
+                <li>encrypted network protocols analysis</li>
+                <li>software binary files structure analysis</li>
+                <li>
+                  decryption of application files (configurations, databases
+                  etc.)
+                </li>{" "}
+                <li>analysis of abandonware and its algorithms</li>
+                <li>custom encryption or compression algorithms recovery</li>
+                <li>modification of closed source application </li>
+                <li>extending current software with custom libraries</li>
+              </ul>
+              <h1 className="text-3xl mb-2 mt-4">
+                Examples of services offered
+              </h1>
             </div>
             <div
               id="div2"
               className={`mb-4 ${activeTab !== 2 ? "hidden" : ""}`}
             >
-              <p>Content of Tab 2</p>
+              <h1 className="text-3xl mb-2 mt-4 text-left">
+                Recovering programming projects
+              </h1>
+              <p className="text-left">
+                I can help you to recover source code, algorithms & resources
+                from the compiled binary <br /> applications written in:
+              </p>
             </div>
             <div
               id="div3"
-              className={`mb-4 ${activeTab !== 3 ? "hidden" : ""}`}
+              className={`mb-4 ${activeTab !== 3 ? "hidden" : "text-left"}`}
             >
-              <p>Content of Tab 3</p>
+              <h1 className="text-3xl mb-2 mt-4 text-left">Experience</h1>
+              <p>
+                I have worked on a special type of software and games
+                localization for over a decade. <br />
+                In most cases my work was done without access to the source
+                codes or technical <br />
+                documentation, just using reverse engineering skills.
+              </p>
             </div>
             <div
               id="div4"
-              className={`mb-4 ${activeTab !== 4 ? "hidden" : ""}`}
+              className={`mb-4 ${activeTab !== 4 ? "hidden" : "text-left"}`}
             >
-              <p>Content of Tab 4</p>
+              <h1 className="text-3xl mb-2 mt-4 text-left">
+                Software protection experience
+              </h1>
+              <p>
+                I have developed software protection products like PELock,
+                .netshrink and Obfuscator.
+                <br /> Over the years I have written many articles for the
+                computer programming press about reverse <br />
+                engineering, copy protection, encryption and software cracking.
+                You can read all of my articles right
+                <br /> here. You can read about my reverse engineering
+                experience and related services like source code <br /> recovery
+                and software localization.
+              </p>
             </div>
           </div>
         </div>
--- src/Reverse/Reverse.js
+++ src/Reverse/Reverse.js
@@ -1,23 +1,9 @@
 import React from "react";
 
 const Reverse = () => {
-  return (
-    <div>
-      <section class="mb-20">
-        <div class="text-center py-24 px-6">
-          <h1 class="text-5xl text-sky-500 md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
-            Reverse Engineering Services
-          </h1>
-          <span class="text-gray-500">
-            Reverse code engineering of binary files & compiled applications,
-            source code recovery, software localization, <br />
-            software licensing & copy protection design.
-          </span>
-        </div>
-      </section>
- 
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 
 export default Reverse;
