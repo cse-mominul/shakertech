0405901574536b64331901ae40c31d83c1473b9d
cse-mominul <cse.mominul.islam>
2023-05-17 18:26:03 +0600

    design update

--- src/Layout/Main/ServicesD.js
+++ src/Layout/Main/ServicesD.js
@@ -24,7 +24,7 @@ const ServicesD = () => {
 
       <section id="div2" className={`mb-4 ${activeTab !== 2 ? "hidden" : ""}`}>
         <div class="text-center py-24 px-6">
-          <h1 class="text-5xl text-sky-500 md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
+          <h1 class="text-5xl text-[#9B59B6] md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
             Source Code Recovery
           </h1>
           <span class="text-gray-500">
@@ -37,7 +37,7 @@ const ServicesD = () => {
 
       <section id="div3" className={`mb-4 ${activeTab !== 3 ? "hidden" : ""}`}>
         <div class="text-center py-24 px-6">
-          <h1 class="text-5xl text-sky-500 md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
+          <h1 class="text-5xl text-[#41BB19] md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
             Software & Games Localization
           </h1>
           <span class="text-gray-500">
@@ -49,12 +49,13 @@ const ServicesD = () => {
 
       <section id="div4" className={`mb-4 ${activeTab !== 4 ? "hidden" : ""}`}>
         <div class="text-center py-24 px-6">
-          <h1 class="text-5xl text-sky-500 md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
+          <h1 class="text-5xl text-[#FE781E] md:text-3xl xl:text-5xl font-bold tracking-tight mb-12">
             Software Protection Design
           </h1>
           <span class="text-gray-500">
             Custom software protection & license key system design for your
-            software, copy protection, anti-cracking & <br/> anti-debug techniques.
+            software, copy protection, anti-cracking & <br /> anti-debug
+            techniques.
           </span>
         </div>
       </section>
@@ -62,29 +63,46 @@ const ServicesD = () => {
       <div class="flex lg:ml-40 lg:mr-40">
         <button
           onClick={() => handleTabClick(1)}
-          className={` ${activeTab !== 1 ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold" : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-sky-500 bg-transparent"}`}
+          className={` ${
+            activeTab !== 1
+              ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold"
+              : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-sky-500 bg-transparent"
+          }`}
         >
           Reverse Engineering
         </button>
         <button
           onClick={() => handleTabClick(2)}
-          className={` ${activeTab !== 2 ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold" : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-[#9B59B6] bg-transparent"}`}
+          className={` ${
+            activeTab !== 2
+              ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold"
+              : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-[#9B59B6] bg-transparent"
+          }`}
         >
           Source Code Recovery
         </button>
         <button
           onClick={() => handleTabClick(3)}
-          class="w-1/5 py-4 px-6 bg-gray-100 border-b-2 border-gray-200 font-semibold"
+          className={` ${
+            activeTab !== 3
+              ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold"
+              : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-[#41BB19] bg-transparent"
+          }`}
         >
           Software Localization
         </button>
         <button
           onClick={() => handleTabClick(4)}
-          class="w-1/5 py-4 px-6 bg-gray-100 border-b-2 border-gray-200 font-semibold"
+          className={` ${
+            activeTab !== 4
+              ? "py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold"
+              : " py-4 px-6 bg-gray-100 border-b-2 border-gray-200 w-1/5 border font-semibold text-[#FE781E] bg-transparent"
+          }`}
         >
           Software Protection Design
         </button>
-        <Link to="/testmonials"
+        <Link
+          to="/testmonials"
           class="w-1/5 py-4 px-6 bg-gray-100 border-b-2 border-gray-200 font-semibold"
         >
           Testimonials
